9171bdba20f72a683a55e42ad76cab249a8f4000
sinchan143054 <sinchan.143054@gmaul.com>
2025-04-24 05:27:43 +0530

    save the file

---
+++ demo.java
@@ -0,0 +1,10 @@
+ public class demo {
+    public static void main(String[] args){
+        int a=20;
+        int b=5;
+        
+        System.out.println("sum is :"+(a+b));
+        System.out.println("subtraction is :"+(a-b));
+        
+    }
+} 
